dc59fede374fbcdbc887c296407c8ef1d9cc4962
hayato-122 <hayato122345@gmail.com>
2026-05-21 11:35:21 +0900

    fix: スマホでのレイアウトずれを修正

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { getImagePath } from "../data/getImagePath";
 export default function Home() {
   return (
     <div>
-      <section className="relative left-1/2 -mt-18.25 min-h-[calc(100svh-64px)] w-screen -translate-x-1/2 overflow-hidden md:min-h-[calc(100svh-320px)]">
+      <section className="relative left-1/2 max-md:-mt-18.25 max-md:min-h-svh w-screen -translate-x-1/2 overflow-hidden md:-mt-18.25 md:min-h-[calc(100svh-380px)]">
         <Image
           src={getImagePath("homepage-top")}
           alt="明石海峡大橋"
@@ -16,9 +16,10 @@ export default function Home() {
           className="object-cover"
           sizes="100vw"
         />
-        <div className="relative z-10 flex min-h-[calc(100svh-64px)] items-center justify-center px-6 md:min-h-[calc(100svh-320px)] md:px-12">
+        <div className="relative z-10 flex max-md:min-h-svh items-center justify-center px-6 py-1 md:min-h-[calc(100svh-380px)] md:px-12">
           <HeroIntro />
         </div>
+        　
       </section>
 
       <section className="pb-16">
